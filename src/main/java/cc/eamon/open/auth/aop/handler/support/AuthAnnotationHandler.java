f8939d54f54d0082ebe3c1a0f84570a601316d1c
Zyh2333 <1245231959@qq.com>
2020-10-22 22:41:37 +0800

    feat: support value(expression) resolve and execute

--- src/main/java/cc/eamon/open/auth/aop/handler/support/AuthAnnotationHandler.java
+++ src/main/java/cc/eamon/open/auth/aop/handler/support/AuthAnnotationHandler.java
@@ -7,6 +7,8 @@ import cc.eamon.open.auth.aop.handler.BaseAnnotationHandler;
 import cc.eamon.open.auth.authenticator.Authenticator;
 import cc.eamon.open.auth.authenticator.AuthenticatorHolder;
 import cc.eamon.open.error.Assert;
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.Expression;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -15,6 +17,9 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Author: eamon
@@ -39,20 +44,36 @@ public class AuthAnnotationHandler extends BaseAnnotationHandler {
         HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
 
         String value = authAnnotation.value();
-        if (StringUtils.isEmpty(value)) {
-            value = request.getRequestURI();
+        boolean execute = false;
+        if (!StringUtils.isEmpty(value)) {
+            // TODO:
+            //表达式校验问题
+            try {
+                Expression expression = AviatorEvaluator.compile(value);
+                List<String> variableNames = expression.getVariableNames();
+                String a2 = null;
+                if(variableNames.size() > 1)
+                    a2 = variableNames.get(1);
+                String a1 = variableNames.get(0);
+
+                Object requestValue = authenticator.getRequestValue(request, response, a1);
+                Object contextValue = authenticator.getContextValue(request, response, a2);
+
+                Map<String, Object> env = new HashMap<>();
+                env.put(a1,requestValue);
+                env.put(a2,contextValue);
+                execute = (Boolean) expression.execute(env);
+            }catch (Exception e){
+                Assert.notNull(null,"EXP_ERROR");
+            }
+
         }
+        //@Auth默认为权限名称
+        else
+            value = request.getRequestURI();
 
         String[] groups = authAnnotation.group();
         Logical[] logicalList = authAnnotation.logical();
-        // TODO:
-        // 0. 获取表达式
-        String exp = authAnnotation.value();
-        // 1. 拿到request参数
-        authenticator.getRequestValue(request, response, "userId");
-        // 2. 拿到header参数
-        authenticator.getRequestValue(request, response, "entityKey");
-        // 3. 校验表达式
 
         boolean result = authenticator.checkPermissions(request, response, value, groups[0]);
         if (groups.length > 1) {
@@ -65,7 +86,7 @@ public class AuthAnnotationHandler extends BaseAnnotationHandler {
                     result = result || authenticator.checkPermissions(request, response, value, groups[i]);
             }
         }
-        Assert.isTrue(result, "NO_AUTH");
+        Assert.isTrue( execute || result, "NO_AUTH");
     }
 }
 
